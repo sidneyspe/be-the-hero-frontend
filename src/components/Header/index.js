f00fc42e399d36567c8dd2a72f765bd64787e3b2
Sidney Pimentel <sidneyspe@gmail.com>
2020-03-26 23:36:33 -0300

    added logic to logout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,21 +6,23 @@ import logoImg from '~/assets/logo.svg';
 
 import { Content, Welcome, ButtonHeader, Logo, GoToButton } from './styles';
 
-import { profileListRequest } from '~/store/modules/profile/actions';
+import { signOut } from '~/store/modules/auth/actions';
 
 export default function Header() {
   const dispatch = useDispatch();
 
   const { name } = useSelector((state) => state.auth.user);
 
-  function handleLogout() {}
+  function handleLogout() {
+    dispatch(signOut());
+  }
 
   return (
     <Content>
       <Logo src={logoImg} alt="Be The Hero" />
       <Welcome>Bem vindo(a), {name}</Welcome>
       <GoToButton to="/incidents/new/">Cadastrar novo caso</GoToButton>
-      <ButtonHeader onClick={handleLogout}>
+      <ButtonHeader type="button" onClick={() => handleLogout()}>
         <FiPower size={18} color="#e02041" />
       </ButtonHeader>
     </Content>
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -4,7 +4,7 @@ import { takeLatest, call, put, all } from 'redux-saga/effects';
 import api from '~/services/api';
 import history from '~/services/history';
 
-import { signInSuccess, signFailure } from './actions';
+import { signInSuccess, signFailure, signOut } from './actions';
 
 export function* signIn({ payload }) {
   try {
@@ -18,15 +18,6 @@ export function* signIn({ payload }) {
 
     const { token, user } = response.data;
 
-    if (user.provider) {
-      alert(
-        'Login inválido, Você não tem permissão para acessar essa aplicação'
-      );
-      return;
-    }
-
-    api.defaults.headers.Authorization = `Bearer ${token}`;
-
     yield put(signInSuccess(token, user));
     history.push('/profile');
   } catch (err) {
@@ -35,19 +26,18 @@ export function* signIn({ payload }) {
   }
 }
 
+export function logout() {
+  history.push('/');
+}
+
 export function setToken({ payload }) {
-  if (!payload) {
-    return;
-  }
-
+  if (!payload) return;
   const { token } = payload.auth;
-
-  if (token) {
-    api.defaults.headers.Authorization = `Bearer ${token}`;
-  }
+  if (token) api.defaults.headers.Authorization = `Bearer ${token}`;
 }
 
 export default all([
   takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_OUT', logout),
 ]);
